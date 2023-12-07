0c0dbd9803087c74c2fb4fdf91f6a34a44179db4
unknown <rjsgnl95@nate.com>
2023-12-07 18:19:01 +0900

    realignment

--- 2023-10-20 day2/VariablelnitializationExample.java
+++
@@ -1,9 +0,0 @@
-package chapter02;
-
-public class VariablelnitializationExample {
-	public static void main(String[] args) {
-		int value = 20;
-		int result = value + 10;
-		System.out.println(result);
-	}
-}
